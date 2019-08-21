05678d2c4b8b329c27813faff51014e8eefc302e
Voloshin <Voloshin@KTU.COM>
2019-08-21 13:33:06 +0300

    first commit

--- CopyFolders/Form1.Designer.cs
+++ CopyFolders/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CopyFolders
 {
-    partial class Form1
+    partial class CopyFiles
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -34,80 +34,122 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(352, 9);
+            this.button1.Location = new System.Drawing.Point(339, 34);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 20);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Source";
+            this.button1.Text = "SelectDir";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(26, 12);
+            this.textBox1.Location = new System.Drawing.Point(26, 34);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(289, 20);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(26, 65);
+            this.textBox2.Location = new System.Drawing.Point(26, 72);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(289, 20);
             this.textBox2.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(352, 61);
+            this.button2.Location = new System.Drawing.Point(338, 72);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(75, 20);
             this.button2.TabIndex = 3;
-            this.button2.Text = "Destination";
+            this.button2.Text = "SelectDir";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(352, 109);
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(339, 111);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 27);
+            this.button3.Size = new System.Drawing.Size(75, 20);
             this.button3.TabIndex = 4;
-            this.button3.Text = "Start";
+            this.button3.Text = "SelectDir";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // progressBar1
+            // textBox3
             // 
-            this.progressBar1.Location = new System.Drawing.Point(26, 112);
-            this.progressBar1.MarqueeAnimationSpeed = 500;
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(289, 23);
-            this.progressBar1.Step = 5;
-            this.progressBar1.TabIndex = 5;
+            this.textBox3.Location = new System.Drawing.Point(26, 112);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(289, 20);
+            this.textBox3.TabIndex = 6;
             // 
-            // timer1
+            // button4
             // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(149, 154);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 26);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Start";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Source";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Control directory";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Destination directory";
+            // 
+            // CopyFiles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(441, 165);
-            this.Controls.Add(this.progressBar1);
+            this.ClientSize = new System.Drawing.Size(425, 204);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "CopyFiles";
+            this.Text = "CopyFiles";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,8 +162,12 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- CopyFolders/Form1.cs
+++ CopyFolders/Form1.cs
@@ -1,41 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CopyFolders
 {
-
-    public partial class Form1 : Form
+    public partial class CopyFiles : Form
     {
-        public Form1()
+        string sourceDir;
+        string sourceDir2;
+        string backupDir;
+        public CopyFiles()
         {
             InitializeComponent();
-
         }
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            if (progressBar1.Value == 100)
-            {
-                timer1.Stop();
-                MessageBox.Show("Copy is Finished");
-            }
-            else
-            {
-                progressBar1.Value = progressBar1.Value + 1;
-            }
-        }
-        string backupDir;
-        string sourceDir; 
-        
-
         void button1_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog source = new FolderBrowserDialog();
@@ -43,7 +20,6 @@ namespace CopyFolders
             {
                 textBox1.Text = source.SelectedPath;
                 sourceDir = source.SelectedPath;
-
             }
         }
         void button2_Click(object sender, EventArgs e)
@@ -52,70 +28,112 @@ namespace CopyFolders
             if (destination.ShowDialog() == DialogResult.OK)
             {
                 textBox2.Text = destination.SelectedPath;
+                sourceDir2 = destination.SelectedPath;
+            } 
+        }
+        void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog destination = new FolderBrowserDialog();
+            if (destination.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = destination.SelectedPath;
                 backupDir = destination.SelectedPath;
             }
         }
-
-        void button3_Click(object sender, EventArgs e)
+        void button4_Click(object sender, EventArgs e)
         {
-            progressBar1.Value = 0;
-            timer1.Start();
             try
             {
-                string[] FilesList = Directory.GetFiles(sourceDir, "*", SearchOption.AllDirectories);
-                /*--------------------------------------------------------------------------*/
-                foreach (string dirPath in Directory.GetDirectories(sourceDir, "*",SearchOption.AllDirectories))
+              int  calcCopyField = 0;
+                sourceDir = textBox1.Text;
+                sourceDir2 = textBox2.Text;
+                backupDir = textBox3.Text;
+                while (true)
                 {
-                    try
+                    if (sourceDir == "")
                     {
-                        Directory.CreateDirectory(dirPath.Replace(sourceDir, backupDir));
+                        MessageBox.Show("Select directory");
+                        button4.Enabled = true;
+                        break;
                     }
-                    catch (IOException copyError)
+                    else if (sourceDir2 == "")
                     {
-                        Console.WriteLine(copyError.Message);
+                        MessageBox.Show("Select directory");
+                        button4.Enabled = true;
+                        break;
                     }
-                }
-                foreach (string f in FilesList)
-                {
-
-                    // Remove path from the file name.
-                    string fName = f.Substring(sourceDir.Length + 1);
-
-                    try
-                    {
-                        // Will not overwrite if the destination file already exists.
-                        File.Copy(Path.Combine(sourceDir, fName), Path.Combine(backupDir, fName), true);
+                     else if (backupDir == "")
+                        {
+                        MessageBox.Show("Select directory");
+                        button4.Enabled = true;
+                        break;
+                        }else { 
+                            string[] FilesList = Directory.GetFiles(sourceDir, "*", SearchOption.AllDirectories);
+                            string[] FilesList2 = Directory.GetFiles(sourceDir2, "*", SearchOption.AllDirectories);
+                            button4.Enabled = false;
+                        foreach (string f in FilesList)
+                        {
+                            string fNames = f.Substring(sourceDir.Length + 1);
+                            string dNames = fNames.Remove(fNames.LastIndexOf('\\') + 1);
+                            string fName = f.Substring(f.LastIndexOf('\\') + 1);
+                            int result = 0;
+                            foreach (string i in FilesList2)
+                            {
+                                // Remove path from the file name.
+                                string fName2 = i.Substring(i.LastIndexOf('\\') + 1);
+                                try
+                                {
+                                    if (Equals(fName, fName2))
+                                    {
+                                        result =  1;
+                                        break;
+                                    }
+                                }
+                                // Catch exception if the file was already copied.
+                                catch (IOException copyError)
+                                {
+                                    Console.WriteLine(copyError.Message);
+                                }
+                            }
+                            try
+                            {
+                                if (Equals(result, 0))
+                                {
+                                    Directory.CreateDirectory(Path.Combine(backupDir, dNames));
+                                    File.Copy(Path.Combine(sourceDir, fNames), Path.Combine(backupDir, fNames), false);
+                                    calcCopyField += 1;
+                                }
+                            }
+                            // Catch exception if the file was already copied.
+                            catch (IOException copyError)
+                            {
+                                
+                                    if (MessageBox.Show(copyError.Message+":  Продолжить копирование?", icon: MessageBoxIcon.Information, caption: "Внимание!", buttons: MessageBoxButtons.OKCancel).Equals(DialogResult.OK))
+                                    {
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+                            }
+                        }
                     }
-
-                    // Catch exception if the file was already copied.
-                    catch (IOException copyError)
-                    {
-                        Console.WriteLine(copyError.Message);
-                    }
+                    button4.Enabled = true;
+                    MessageBox.Show("Скопировано: " + calcCopyField+" файлов");
+                    break;
                 }
             }
-
-            //foreach (string f in txtList)
-            //{
-            //   File.Delete(f);
-            //}
-            // }
             catch (DirectoryNotFoundException dirNotFound)
             {
-                Console.WriteLine(dirNotFound.Message);
+                MessageBox.Show(dirNotFound.Message, icon: MessageBoxIcon.Information, caption: "Внимание!", buttons: MessageBoxButtons.OK);
+                
             }
-
         }
-
-       
     }
 }
-    
+
     
 
-                    
-
-            
-        
     
 
